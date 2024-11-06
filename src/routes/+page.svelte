39cc0cff2207e8b3db09ee10844b588f2366899d
VincentBriand44 <pro.vincent.briand@pm.me>
2024-11-06 09:49:42 +0100

    :sparkles: (client) feature logic implement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,47 @@
 <script lang="ts">
+import { enhance } from '$app/forms';
+import type { Role } from '$lib/schemas';
 import { cn } from '$lib/utils';
+import type { InferInput } from 'valibot';
+import type { ActionData, PageData } from './$types';
 
-const grid = $state<(Player | undefined)[]>(Array(9))
+let grid = $state<(InferInput<typeof Role> | '')[]>(Array(9));
+let selected = $state<number>();
+let { form }: { data: PageData; form: ActionData } = $props();
 
-type Player = 'X' | 'O'
+$effect(() => {
+  if (!form?.grid) return;
 
-let player = $state<Player>('X')
-let playable = $state<boolean>(true)
-let winner = $state<Player | undefined>()
-
-const handleClick = (index: number) => {
-  if (!playable || winner) return
-  if (grid[index]) return
-
-  playable = false
-  grid[index] = player
-  
-  // Single player
-  playable = true
-  player = player === 'X' ? 'O' : 'X'
-
-  // Check victory
-  for(let i = 0; i < 3; i++) {
-    const line = i * 3
-    
-    if (
-      (grid[line] === grid[line+1] && grid[line] === grid[line+2])||
-      (grid[line] === grid[line+3] && grid[line] === grid[line+6])
-    ) {
-      winner = grid[line]
-    }  
-  }
-  if (
-    (grid[0] === grid[4] && grid[0] === grid[8])||
-    (grid[2] === grid[4] && grid[4] === grid[6])
-  ) {
-    winner = grid[4]
-  }
-
-  if (winner) {
-    console.info(`${winner} a gagné !`)
-  }
-}
+  grid = form.grid;
+});
 </script>
 
-<div class="grid grid-cols-3 grid-rows-3 w-80 h-80 relative">
+<form action="?/start" method="POST" use:enhance>
+  <select name="role" required>
+    <option value="X">X</option>
+    <option value="O">O</option>
+  </select>
+  <button type="submit" class="text-slate-500">
+    Commencer
+  </button>
+</form>
+
+<form class="grid grid-cols-3 grid-rows-3 w-80 h-80 relative" method="POST" action="?/try" use:enhance>
   {#each grid as item, index}
     <button
       class={cn(
         index % 2 === 0 ? 'bg-slate-800' : 'bg-slate-700',
         "flex items-center justify-center text-6xl font-bold text-slate-500"
       )}
-      onclick={()=>handleClick(index)}
-      >
+      type="submit"
+      onclick={(e) => {
+        selected = index
+      }}>
       {item}
     </button>
-  {/each}
-  {#if winner}
-    <h1 class="absolute self-center place-self-center font-bold text-4xl text-slate-900"> Le gagnant est {winner}</h1>
+    {/each}
+    <input type="hidden" name="pos" value={selected}>
+  {#if form?.message}
+    <p>{form.message}</p>
   {/if}
-</div>
+</form>
